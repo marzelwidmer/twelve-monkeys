d818486563f0118f423943f6aded3a85767b7c1d
Marcel Widmer <marzelwidmer@gmail.com>
2016-08-14 22:12:55 +0200

    init commit

---
+++ src/main/java/ch/keepcalm/web/TwelveMonkeysApplication.java
@@ -0,0 +1,12 @@
+package ch.keepcalm.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveMonkeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwelveMonkeysApplication.class, args);
+	}
+}
